0de5ae93677eb149b91db4cd9374f3a0cf94c6e3
costantino2000 <86307814+costantino2000@users.noreply.github.com>
2023-05-18 18:39:34 +0200

    First upload

---
+++ README.md
@@ -0,0 +1,40 @@
+Instructions:
+
+- Open your desired minecraft version (from the "version" folder) .jar file with a zip program;
+- Extract the "recipes" folder from inside ```data\minecraft``` into the "advancements" datapack folder (where the python script is located) and rename it "recipes2";
+- Extract the "recipes" folder from inside ```data\minecraft\advancements``` into the same datapack folder;
+- Open the terminal from inside the datapack folder and run the script with ```python convert_recipes.py``` (pyhton3 if you are on linux);
+- Delete the "recipes2" folder.
+
+Note: make sure use the terminal to run the script from the correct directory, if you have a subfolder named ```recipes``` in the location you are executing the script from (for example your home directory if you open the script directly with VScode and use Code Runner), every .json file present there will be overwitten!
+
+The script will change every item recipe to this:
+```
+{
+    "parent":"minecraft:recipes/root",
+    "criteria":{
+        "has_ingredients":{
+            "trigger":"minecraft:inventory_changed",
+            "conditions":{
+                "items":[
+                    {
+                        (required_items)
+                    }
+                ]
+            }
+        }
+    },
+    "requirements":[
+        [
+            "has_ingredients"
+        ]
+    ],
+    "rewards":{
+        "recipes":[
+            "minecraft:item_name"
+        ]
+    }
+}
+```
+
+And that's it, the datapack is ready! The script should also work with future versions of the game (except if they change the recipe and advancement structure). You can change the ```pack_format``` version from the ```pack.mcmeta``` file if you don't want to get a warning when selecting the datapack.
---
+++ more_accurate_recipe-book_1_19_4/data/minecraft/advancements/convert_recipes.py
@@ -0,0 +1,132 @@
+import os, json
+
+
+def extract_ingredients_shapeless(data):
+    ingredients = []
+    for item in data["ingredients"]:
+        if isinstance(item,list):
+            #if there is a list the items inside it are funglible, and would need to be put together in the advancement:
+            ingredient = {"items":[]}
+            for element in item:
+                ingredient["items"].append(element["item"])
+            ingredients.append(ingredient)
+            print(ingredient)
+        else:
+            ingredient = {}
+            if "tag" in item:
+                ingredient["tag"] = item["tag"]
+            else:
+                ingredient["items"] = [item["item"]]
+            ingredients.append(ingredient)
+            print(ingredient)
+    return ingredients
+
+
+def remove_duplicates(ingredients):
+    unique_ingredients = []
+    for ingredient in ingredients:
+        if ingredient not in unique_ingredients:
+            unique_ingredients.append(ingredient)
+    return unique_ingredients
+    
+
+def extract_ingredients_other(data, item_name, parent=None):
+    ingredients = []
+    #if it finds an item or tag it will add it to the ingredients, else it will keep searching:
+    if isinstance(data, dict):
+        for key, value in data.items():
+            if key == "item" or key == "tag":
+                #if the item found is not the result of the recipe it gets added to the ingredients list:
+                if(value[10:] != item_name):    #(excluding the "minecraft:" prefix in order to only get the item name)
+                    ingredient = [key, value, parent]
+                    ingredients.append(ingredient)
+                    print(ingredient)
+            elif isinstance(value, (dict, list)):
+                ingredients.extend(extract_ingredients_other(value, item_name, parent=key))
+    elif isinstance(data, list):
+        for element in data:
+            ingredients.extend(extract_ingredients_other(element, item_name, parent=parent))
+    return ingredients
+
+
+def merge_fungible_ingredients(list):
+    ingredients_by_symbol = {}
+    resulting_list = []
+    for type, item, symbol in list:
+        #if two items have the same symbol in the recipe (for example "#") they will be put together as fungible
+        if symbol in ingredients_by_symbol:
+            ingredients_by_symbol[symbol][1].append(item)
+        else:
+            ingredients_by_symbol[symbol] = [type, [item]]
+    for values in ingredients_by_symbol.values():
+        #the items are finally taken out from the dictionary and put inside a list:
+        resulting_list.append([values[0], values[1]])
+    return resulting_list
+
+
+if __name__ == "__main__":
+    print(os.getcwd())  #print current directory
+    for root, dirs, files in os.walk("./recipes"):
+        for file in files:  #search the recipes folder for every recipe advancement
+            if file.endswith(".json"):
+                filepath = os.path.join(root, file)
+                print("\n-", file)
+
+                #search for the corresponding file in the actual recipes folder in order to extract the ingredients
+                filepath2 = os.path.join("./recipes2", file)
+                with open(filepath2, "r") as f:
+                    data = json.load(f)
+                    ingredients = []
+                    if data["type"] == "minecraft:crafting_shapeless":
+                        print("type: crafting_shapeless")
+                        ingredients = extract_ingredients_shapeless(data)
+                        ingredients = remove_duplicates(ingredients)
+                    else:
+                        print("type:", data["type"][10:])
+                        ingredients = extract_ingredients_other(data, file[:len(file)-5]) #excludes the .json extension and passes the name of the file
+                        ingredients = merge_fungible_ingredients(ingredients)
+
+                if file.startswith("root"):
+                    os.remove(filepath) #(the root.json file is not needed)
+                else:
+                    with open(filepath, 'w') as f:
+                        advancement = {
+                            "parent": "minecraft:recipes/root",
+                            "criteria": {
+                                "has_ingredients": {
+                                    "trigger": "minecraft:inventory_changed",
+                                    "conditions": {
+                                        "items": [
+                                            #crafting ingredients will go here
+                                        ]
+                                    }
+                                }
+                            },
+                            "requirements": [
+                                [
+                                    "has_ingredients"
+                                ]
+                            ],
+                            "rewards": {
+                                "recipes": [
+                                    "minecraft:{}".format(file[:len(file)-5])
+                                ]
+                            }
+                        }
+                        
+                        items = advancement["criteria"]["has_ingredients"]["conditions"]["items"]
+                        
+                        #the ingredients previously extracted from the recipe file are added to the advancement:
+                        if data["type"] == "minecraft:crafting_shapeless":
+                            for item in ingredients:
+                                items.append(item)
+                        else:
+                            for item in ingredients:
+                                if item[0] == "item":
+                                    ingredient = { "items": item[1] }
+                                elif item[0] == "tag":
+                                    ingredient = { "tag": item[1][0] }  #(the tag does not need to be inside a list)
+                                items.append(ingredient)
+
+                        json_string = json.dumps(advancement, indent=2)
+                        f.write(json_string)
